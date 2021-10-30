434b964eddbc16a9b1c9a92038ff37df7cfeea4d
maximtolstikov <tolstikov.maxim.vadimovich@gmail.com>
2021-10-30 22:01:11 +0300

    Add the app icons

--- ARGame/App/AppDelegate.swift
+++ ARGame/App/AppDelegate.swift
@@ -13,11 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        setupWindow()
+        runFlow()
         return true
     }
     
-    private func setupWindow() {
+    private func runFlow() {
         self.window = window ?? UIWindow()
         let gameViewController = ModuleFactory.buildStartScreen()
         window?.rootViewController = UINavigationController(rootViewController: gameViewController)
